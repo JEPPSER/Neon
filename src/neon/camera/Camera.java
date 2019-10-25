aef33c7050ebe0cbd642810b9f191eca90b3e9b1
Jesper Bergström <jesper.b96@gmail.com>
2019-10-25 22:47:02 +0200

    Improves camera vertical scrolling

--- src/neon/camera/Camera.java
+++ src/neon/camera/Camera.java
@@ -1,6 +1,5 @@
 package neon.camera;
 
-import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 
@@ -11,9 +10,14 @@ public class Camera {
 	
 	private ControllableEntity focusedEntity;
 	private GameContainer gc;
+	private float offsetX;
+	private float offsetY;
+	private float yScrollValue = 0.2f;
 	
 	public Camera(ControllableEntity focusedEntity, GameContainer gc) {
 		this.focusedEntity = focusedEntity;
+		offsetX = focusedEntity.getX() * -1 + gc.getWidth() / 2;
+		offsetY = focusedEntity.getY() * -1 + gc.getHeight() / 2;
 		this.gc = gc;
 	}
 	
@@ -26,10 +30,17 @@ public class Camera {
 	}
 	
 	public void renderPlayField(Level level, Graphics g) {
-		float offsetX = focusedEntity.getX() * -1 + gc.getWidth() / 2;
-		float offsetY = focusedEntity.getY() * -1 + gc.getHeight() / 2;
-		g.setColor(Color.green);
-		//g.drawRect(offsetX, offsetY, level.getWidth(), level.getHeight());
+		offsetX = focusedEntity.getX() * -1 + gc.getWidth() / 2;
+		float y = focusedEntity.getY() + offsetY;
+		float height = focusedEntity.getCollision().getHitbox().getHeight();
+		
+		// Scroll screen vertically
+		if (y > gc.getHeight() - height - gc.getHeight() * yScrollValue) {
+			offsetY = -focusedEntity.getY() + gc.getHeight() - height - gc.getHeight() * yScrollValue;
+		} else if (y < gc.getHeight() * yScrollValue) {
+			offsetY = -focusedEntity.getY() + gc.getHeight() * yScrollValue;
+		}
+		
 		for (int i = 0; i < level.getObjects().size(); i++) {
 			level.getObjects().get(i).render(g, offsetX, offsetY);
 		}
