87df03fd4ec4b6b7666ae26b629c4178f2aaa459
Jesper Bergström <jesper.b96@gmail.com>
2019-12-11 22:36:41 +0100

    Added new player idle sprite (new concept)

--- src/neon/entity/controllable/Player.java
+++ src/neon/entity/controllable/Player.java
@@ -42,7 +42,7 @@ public class Player extends ControllableEntity {
 
 	public Player(float x, float y, Weapon weapon) {
 		name = "Player";
-		color = Color.yellow;
+		color = Color.white;
 		this.physics = new Physics(0f, 0f);
 		this.collision = new Collision(new Rectangle(0, 0, 50, 100), 1.0f, 10f, true);
 		this.x = x;
